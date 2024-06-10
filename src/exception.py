3702de8a3502c6b186cf327c7ec28d60564b4013
adhitya0710 <adhitya.07.14@gmail.com>
2024-06-11 01:15:00 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,13 +1,15 @@
 import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
 from src.logger import logging
+
 
 
 def error_message_details(error,error_details:sys):
     _,_,exc_tb=error_details.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_message="Error occured in script name [{0}], line number [{1}], error message [{2}]".format(
-        file_name,exc_tb.tb_lineno,str(error)
-    )   
+        file_name,exc_tb.tb_lineno,str(error))   
 
     return error_message
 
@@ -18,3 +20,6 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+
+
+
